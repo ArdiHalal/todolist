33195b98a5adb868ea0e2c802b39c9ad51149710
ArdiHalal <arditaa744@gmail.com>
2025-02-11 15:48:41 +0700

    ardi|021125|update title

--- index2.php
+++ index2.php
@@ -10,13 +10,13 @@ $user_id = $_SESSION['user_id'];
 $q_lists = "SELECT * FROM lists WHERE user_id='$user_id'";
 $run_q_lists = mysqli_query($conn_todolist, $q_lists);
 ?>
-    
+
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To-Do List ardita</title>
+    <title>To-Do List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style1.css">
     <script>
